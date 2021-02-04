181987867b7bede789708b880fb44bbf2cecefb6
MaiGao886 <1309473883@qq.com>
2021-02-04 10:27:32 +0800

    commit 7

---
+++ test/src/leetcode_1_7/Solution.java
@@ -0,0 +1,38 @@
+package leetcode_1_7;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int reverse = solution.reverse(-19999999);
+        System.out.println(reverse);
+        System.out.println(Integer.MIN_VALUE);
+    }
+
+    public int reverse(int x) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        boolean isFushu = false;
+        if (x < 0) {
+            isFushu = true;
+            x = Math.abs(x);
+        }
+
+        while (x / 10 > 0 || x % 10 > 0) {
+            int i = x % 10;
+            x /= 10;
+            integers.add(i);
+        }
+        long a = 0;
+        for (int i = 0; i < integers.size(); i++) {
+            a += (integers.get(i) * Math.pow(10, integers.size() - 1 - i));
+        }
+        if (isFushu) {
+            a = -a;
+        }
+        if (a > Math.pow(2, 31) - 1 || a < -Math.pow(2, 31)) {
+            a = 0;
+        }
+        return (int) a;
+    }
+}
